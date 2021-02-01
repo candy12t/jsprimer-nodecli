7b9a43d7bb781230eb36e423238a4cf0bbdc1679
Takumi Kanada <candy12t@gmail.com>
2021-02-01 13:37:43 +0900

    feat: md to html

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const program = require("commander");
 const fs = require("fs");
+const marked = require("marked");
+
 
 program.parse(process.argv);
 const filePath = program.args[0];
+
 
 fs.readFile(filePath, 'utf8', (err, file) => {
   if (err) {
@@ -10,5 +13,6 @@ fs.readFile(filePath, 'utf8', (err, file) => {
     process.exit(1);
     return;
   }
-  console.log(file);
+  const html = marked(file);
+  console.log(html);
 });
