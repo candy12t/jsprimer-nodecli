5dc5d1f0191386b5902bdfb104df94790cf3a53a
Takumi Kanada <candy12t@gmail.com>
2021-02-01 13:29:59 +0900

    feat: error handling

--- main.js
+++ main.js
@@ -5,5 +5,10 @@ program.parse(process.argv);
 const filePath = program.args[0];
 
 fs.readFile(filePath, 'utf8', (err, file) => {
+  if (err) {
+    console.error(err.message);
+    process.exit(1);
+    return;
+  }
   console.log(file);
 });
