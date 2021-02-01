e84a91e9e7eb8bfbada58c5a6c6ac69a0dbb95bd
Takumi Kanada <candy12t@gmail.com>
2021-02-01 13:25:33 +0900

    feat: readfile with 'fs.readFile'

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const program = require("commander");
+const fs = require("fs");
 
 program.parse(process.argv);
-
-
 const filePath = program.args[0];
-console.log(filePath);
+
+fs.readFile(filePath, 'utf8', (err, file) => {
+  console.log(file);
+});
